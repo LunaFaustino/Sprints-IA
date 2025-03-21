cffd085cfaef42e3d183c201bec6c68b27f47a50
Luna Faustino <lu.faustino2000@gmail.com>
2025-03-20 21:10:43 -0300

    feat: adicionando codigo fonte da aplicação react-nativa

---
+++ challenge-odontoprev/App.js
@@ -0,0 +1,26 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { StyleSheet } from 'react-native';
+import Home from './screens/Home';
+import EnviarRaioX from './screens/EnviarRaioX';
+import TelaAnalises from './screens/TelaAnalises';
+import ImportarRaio from './screens/TelaImportar';
+import TelaPergunta from './screens/TelaPergunta';
+
+const Stack = createNativeStackNavigator()
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{
+        headerShown: false
+      }}>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="EnviarRaio" component={EnviarRaioX} />
+        <Stack.Screen name="TelaImportar" component={ImportarRaio} />
+        <Stack.Screen name="TelaAnalises" component={TelaAnalises} />
+        <Stack.Screen name="TelaPergunta" component={TelaPergunta} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
